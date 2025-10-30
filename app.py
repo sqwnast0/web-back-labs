ab7429d014f7facf978f10c86bb970edc37a3bb5
Чикирисова Анастасия <acikirisova@gmail.com>
2025-10-30 16:19:38 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,10 +2,27 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/web")
-def start():
+def web():
     return """<!doctype html>
         <html>
             <body>
                 <h1>web-сервер на flask</h1>
+                <a href="/authot">author</a>
+            </body>
+        </html>"""
+
+@app.route("/author")
+def author():
+    name = "Чикирисова Анастасия Вячеславовна"
+    group = "ФБИ-33"
+    faculty = "ФБ"
+    
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
             </body>
         </html>"""
