4972f8b31b2425a61ca2ab309ce8e766921b6da0
Чикирисова Анастасия <acikirisova@gmail.com>
2025-10-30 16:11:58 +0700

    Три кавычки(2)

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/web")
 def start():
-    return "<!doctype html>" \
-        "<html>" \
-        "    <body>" \
-        "        <h1>web-сервер на flask</h1>" \
-        "    </body>" \
-        "</html>"""
+    return """<!doctype html>
+        <html>
+            <body>
+                <h1>web-сервер на flask</h1>
+            </body>
+        </html>"""
