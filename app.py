5d8bf21c29b9341bf6d68609ff105c2b8d8bad7f
Чикирисова Анастасия <acikirisova@gmail.com>
2025-10-31 23:45:14 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ def lab1():
             <body>
                 <ul>
                     <h1>Лабораторная работа 1</h1>
-                    <li><a href="/author">author</a></li>
-                    <li><a href="/web">web</a></li>
-                    <li><a href="/image">image</a></li>
-                    <li><a href="/counter">counter</a></li>
+                    <li><a href="/lab1/author">author</a></li>
+                    <li><a href="/lab1/web">web</a></li>
+                    <li><a href="/lab1/image">image</a></li>
+                    <li><a href="/lab1/counter">counter</a></li>
                 </ul>
             </body>
         </html>"""    
@@ -53,7 +53,7 @@ def author():
         </html>"""
 
 
-@app.route('/image') 
+@app.route('/lab1/image') 
 def image():
     path = url_for("static", filename="oak.jpg")
     css_path = url_for("static", filename="lab1.css")
@@ -72,7 +72,7 @@ def image():
 
 count = 0
 
-@app.route('/counter') 
+@app.route('/lab1/counter') 
 def counter():
     global count
     count += 1
@@ -100,7 +100,7 @@ def reset_counter():
     count = 0
     return redirect('/counter')
 
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
     return redirect("/author")
 
