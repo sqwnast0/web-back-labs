b0087b16e2b00c799651210afaba4d7e8a5b3ebc
Чикирисова Анастасия <acikirisova@gmail.com>
2025-11-25 23:10:57 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template, request, make_response, redirect
 lab3 = Blueprint('lab3', __name__)
 
+
 @lab3.route('/lab3/')
 def lab():
     name = request.cookies.get('name')
     name_color = request.cookies.get('name_color')
     return render_template('/lab3/lab3.html', name=name, name_color=name_color)
+
 
 @lab3.route('/lab3/cookie')
 def cookie():
@@ -15,6 +17,7 @@ def cookie():
     resp.set_cookie('name_color', 'magenta')
     return resp
 
+
 @lab3.route('/lab3/del_cookie')
 def del_cookie():
     resp = make_response(redirect('/lab3/'))
@@ -22,6 +25,7 @@ def del_cookie():
     resp.delete_cookie('age')
     resp.delete_cookie('name_color')
     return resp
+
 
 @lab3.route('/lab3/form1')
 def form1():
@@ -34,3 +38,38 @@ def form1():
         errors['age'] = 'Заполните поле!'
     sex = request.args.get('sex')
     return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
+
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('lab3/order.html')
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    drink_name = ''
+
+    if drink == 'coffee':
+        price = 120
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+
+    additions = []
+    if request.args.get('milk') == 'on':
+        price += 30
+        additions.append('молоко')
+    if request.args.get('sugar') == 'on':
+        price += 10
+        additions.append('сахар')
+    
+    return render_template('lab3/pay.html', price=price, drink_name=drink_name, additions=additions)
+
+
+@lab3.route('/lab3/success')
+def success():
+    price = request.args.get('price', 0)
+    return render_template('lab3/success.html', price=price)
