9b53e34f66b1ca391ee331adca18f171547a2f82
sqwnast0 <anastasiatchikirisova@yandex.ru>
2025-12-29 17:18:56 +0700

    8 Лабораторная работа

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 
 app = Flask(__name__)
 
@@ -27,6 +28,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 
 access_log = []
 
